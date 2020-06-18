d8bcb5edcdb90aeceaed5b13d34a1b54a621c5ad
tommyrharper <tommyrharper@gmail.com>
2020-06-18 20:18:08 +0100

    Create DataInput class

--- src/components/DataInput.js
+++ src/components/DataInput.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class DataInput extends Component {
-  render() {
-    return(
-      <div>
-        <label htmlfor="startingCash">Starting Cash:</label>
-        <input type="text" />
-        <br/>
-        <label htmlfor="startingCash">Interest Rate</label>
-        <input type="text" />
-        <input className="btn" type="submit" value="Create Graph" />
-      </div>
-    ) 
+class DataInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formValues: {}
+    }
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    let formValues = this.state.formValues;
+    let name = event.target.name;
+    let value = event.target.value;
+
+    formValues[name] = value;
+
+    this.setState({formValues})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert("Name: " + this.state.formValues.name + " Email: " + this.state.formValues.email);
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <label> Starting Cash:
+          <input type="text" name="startingCash" placeholder="10000 = £10,000" value={this.state.formValues["name"]} onChange={this.handleChange.bind(this)} />
+        </label><br />
+        <label> ROI:
+          <input type="text" name="ROI" placeholder="0.1 = 10%" value={this.state.formValues["email"]} onChange={this.handleChange.bind(this)}/>
+        </label><br />
+        <input className="btn btn-primary" type="submit" value="Submit" />
+      </form>
+    )
   }
 }
 
